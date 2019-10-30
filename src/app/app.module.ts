f0e0433e6c66483d317b45219630bec95a280e14
Phillip Dodd <phillipdodd@gmail.com>
2019-10-30 15:03:23 -0400

    separated footer and toolbar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
