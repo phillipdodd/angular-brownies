c3b0f20b171e04e528db81de6aa6526d5036a022
Phillip Dodd <pdodd@uga.edu>
2019-11-01 14:33:24 -0400

    injected firestore into pagea

--- src/app/pagea/pagea.component.ts
+++ src/app/pagea/pagea.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from "rxjs";
 @Component({
   selector: 'app-pagea',
   templateUrl: './pagea.component.html',
   styleUrls: ['./pagea.component.css']
 })
 export class PageaComponent implements OnInit {
-
-  constructor() { }
+  items: Observable<any[]>
+  constructor(db: AngularFirestore) {
+    this.items = db.collection('items').valueChanges();
+  }
 
   ngOnInit() {
   }
